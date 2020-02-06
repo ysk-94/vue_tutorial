8c9fe24fc6ae9fc475361878f75cf74a4c7ff81c
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-02-06 14:37:29 +0900

    はじめに 完了

--- index.js
+++ index.js
@@ -19,3 +19,49 @@ var app3 = new Vue({
         seen: true
     }
 });
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello vue.js'
+    },
+    methods: {
+        reserveMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' },
+        ]
+    }
+})
